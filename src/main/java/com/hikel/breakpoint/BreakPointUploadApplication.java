94cb3efb3dfd8667b3fb25a6cbe80871ca6b2e5a
hikel9527 <jhj358100@163.com>
2026-02-22 00:02:45 +0800

    初始化项目

---
+++ src/main/java/com/hikel/breakpoint/BreakPointUploadApplication.java
@@ -0,0 +1,12 @@
+package com.hikel.breakpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreakPointUploadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BreakPointUploadApplication.class,args);
+
+    }
+}
